852af32e61dafdeb8161137c472042f7d74b2b96
Projjal Paul <projjal2002@gmail.com>
2026-05-01 14:46:02 +0530

    patch4

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-    <SpeedInsights />
-    <Analytics />
   </BrowserRouter>,
 );
