15adec0e83958d439abeff5c188f6eedf67c7ebe
Projjal Paul <projjal2002@gmail.com>
2026-05-02 08:29:24 +0530

    patch12

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "`https://real-time-interview-prep-project.onrender.com",
+  "https://real-time-interview-prep-project.onrender.com",
   {
     transports: ["websocket"],
     autoConnect: true,
